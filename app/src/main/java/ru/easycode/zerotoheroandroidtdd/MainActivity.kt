c6015ebf9374bce8ff5d369347670e4e0bd07ccf
antrdima <antrdima@gmail.com>
2024-03-08 17:26:16 +0500

    solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -7,10 +7,31 @@ import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val adapter = TextAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.recyclerView.adapter = adapter
+
+        binding.actionButton.setOnClickListener {
+            adapter.addItem(binding.inputEditText.text.toString())
+            binding.inputEditText.setText("")
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putStringArrayList("key", adapter.getItems())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val data = savedInstanceState.getStringArrayList("key")
+        data?.forEach{
+            adapter.addItem(it)
+        }
     }
 }
